a84cf8b7cd2215cb3a24d02a4749dff274d12331
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2021-08-19 17:38:13 -0300

    Delete 02 Site/site directory

--- 02 Site/site/app/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $url = "http://169.254.169.254/latest/meta-data/instance-id";
-    $instance_id = file_get_contents($url);
-    echo " <h2> <font color='blue'> Instance ID: <b>" . $instance_id . "</b><br/></font> </h2> ";
-?>
